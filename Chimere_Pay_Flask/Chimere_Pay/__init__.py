1a33d2efaf870253e594159c7b39bb3f9c754be4
sidhantha <sidhantha_poddar@mckinsey.com>
2023-03-09 02:12:46 +0530

    added db and features

---
+++ Chimere_Pay_Flask/Chimere_Pay/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import os
+import psycopg2.pool
+
+from datetime import timedelta
+
+
+
+
+
+app                                     = Flask(__name__)
+
+SECRET_KEY                              = os.urandom(32)
+app.config['SECRET_KEY']                = SECRET_KEY
+
+
+
+
+
+# app.config["Connection Pool"]           = pool
+
+
+
+from Chimere_Pay import routes
